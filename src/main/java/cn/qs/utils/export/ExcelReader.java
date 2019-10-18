eba1e62d679439d4a55e2b78a6af6641797e767e
qiaozhi <qiao_liqiang@163.com>
2019-10-18 22:50:30 +0800

    修改excel读取工具栏

--- src/main/java/cn/qs/utils/export/ExcelReader.java
+++ src/main/java/cn/qs/utils/export/ExcelReader.java
@@ -3,16 +3,20 @@ package cn.qs.utils.export;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.text.NumberFormat;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -21,12 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExcelReader {
-
-	public static void main(String[] args) {
-		ExcelReader excelExporter = new ExcelReader("C:\\Users\\Administrator\\Desktop\\bs\\41232\\ttt.xlsx");
-		List<Map<String, Object>> readAllSheetDatas = excelExporter.readSheetDatas(4);
-		System.out.println(readAllSheetDatas);
-	}
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExcelReader.class);
 
@@ -41,32 +39,32 @@ public class ExcelReader {
 		// 解决版本问题，HSSFWorkbook是97-03版本的xls版本，XSSFWorkbook是07版本的xlsx
 		try {
 			workBook = new XSSFWorkbook(new FileInputStream(file));
+			LOGGER.debug("是03版本的excel，采用XSSFWorkbook读取");
 		} catch (Exception e) {
 			try {
 				workBook = new HSSFWorkbook(new FileInputStream(file));
+				LOGGER.debug("是07版本的excel，采用HSSFWorkbook读取");
 			} catch (Exception e1) {
 				LOGGER.error("Excel格式不正确", e1);
 				throw new RuntimeException(e1);
 			}
 		}
-
 	}
 
 	public ExcelReader(InputStream inputStream) {
 		// 解决版本问题，HSSFWorkbook是97-03版本的xls版本，XSSFWorkbook是07版本的xlsx
 		try {
 			workBook = new XSSFWorkbook(inputStream);
+			LOGGER.debug("是03版本的excel，采用XSSFWorkbook读取");
 		} catch (Exception e) {
 			try {
 				workBook = new HSSFWorkbook(inputStream);
+				LOGGER.debug("是07版本的excel，采用HSSFWorkbook读取");
 			} catch (Exception e1) {
 				LOGGER.error("Excel格式不正确", e1);
 				throw new RuntimeException(e1);
 			}
-		} finally {
-			IOUtils.closeQuietly(inputStream);
 		}
-
 	}
 
 	/**
@@ -121,13 +119,24 @@ public class ExcelReader {
 		}
 
 		Sheet sheet = workBook.getSheetAt(sheetIndex);
-		return readSheetDatas(sheetIndex, sheetHeaders.get(sheet));
+		return readSheetDatas(sheetIndex, sheetHeaders.get(sheet), 1);
 	}
 
 	public List<Map<String, Object>> readSheetDatas(int sheetIndex, String[] headers) {
 		return readSheetDatas(sheetIndex, headers, 0);
 	}
 
+	/**
+	 * 读取指定sheet数据
+	 * 
+	 * @param sheetIndex
+	 *            sheet的下标
+	 * @param headers
+	 *            表头
+	 * @param startRow
+	 *            起始行数
+	 * @return
+	 */
 	public List<Map<String, Object>> readSheetDatas(int sheetIndex, String[] headers, int startRow) {
 		List<Map<String, Object>> result = new LinkedList<>();
 		if (ArrayUtils.isEmpty(headers)) {
@@ -149,11 +158,10 @@ public class ExcelReader {
 			Map<String, Object> rowMap = new LinkedHashMap<>();
 			result.add(rowMap);
 			for (int j = 0, length_1 = headers.length; j < length_1; j++) {
-				String cellValue = getCellValue(row.getCell(j));
+				String cellValue = getCellStringValue(row.getCell(j));
 				String header = headers[j];
 				rowMap.put(header, cellValue);
 			}
-
 		}
 
 		return result;
@@ -169,12 +177,44 @@ public class ExcelReader {
 	 * @return
 	 */
 	private String getCellValue(Cell cell) {
-		if (cell != null) {
-			cell.setCellType(cell.CELL_TYPE_STRING);
-			return cell.getStringCellValue();
+		if (cell == null) {
+			return "";
 		}
 
-		return "";
+		cell.setCellType(CellType.STRING);
+		return StringUtils.defaultIfBlank(cell.getStringCellValue(), "");
+	}
+
+	/**
+	 * POI3.15之后的读取方法(建议用这个)
+	 * 
+	 * @param cell
+	 * @return
+	 */
+	private String getCellStringValue(Cell cell) {
+		if (cell == null) {
+			return "";
+		}
+
+		String cellValue = null;
+		if (cell.getCellTypeEnum() == CellType.NUMERIC) {
+			if (HSSFDateUtil.isCellDateFormatted(cell)) {
+				cellValue = DateFormatUtils.format(cell.getDateCellValue(), "yyyy-MM-dd");
+			} else {
+				NumberFormat nf = NumberFormat.getInstance();
+				cellValue = String.valueOf(nf.format(cell.getNumericCellValue())).replace(",", "");
+			}
+		} else if (cell.getCellTypeEnum() == CellType.STRING) {
+			cellValue = cell.getStringCellValue();
+		} else if (cell.getCellTypeEnum() == CellType.BOOLEAN) {
+			cellValue = String.valueOf(cell.getBooleanCellValue());
+		} else if (cell.getCellTypeEnum() == CellType.ERROR) {
+			cellValue = "错误类型";
+		} else {
+			cellValue = "";
+		}
+
+		return cellValue;
 	}
 
 }
